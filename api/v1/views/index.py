4ff22b07b274ae19008684418a1470666a5097cc
victor koech <kipkiruiv408@gmail.com>
2023-02-15 09:06:37 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Returns JSON status on object app_views
 """
 from flask import Flask
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -11,3 +12,22 @@ def status():
     """ Returns status message in JSON
     """
     return ({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """ Returns stats message in JSON
+    """
+    amens = storage.count("Amenity")
+    cities = storage.count("City")
+    places = storage.count("Place")
+    reviews = storage.count("Review")
+    states = storage.count("State")
+    users = storage.count("User")
+
+    return ({"amenities": amens,
+             "cities": cities,
+             "places": places,
+             "reviews": reviews,
+             "states": states,
+             "users": users})
