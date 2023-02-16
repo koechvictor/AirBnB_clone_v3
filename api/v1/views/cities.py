a393db6eae756de6bc958e882754920a204e07c5
victor koech <kipkiruiv408@gmail.com>
2023-02-16 07:37:51 +0000

    update

---
+++ api/v1/views/cities.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Creates a new view for State objects for all default API actions
+"""
+from flask import Flask, request, jsonify
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+
+
+def getcity(city):
+    return (city.to_dict(), 200)
+
+
+def putcity(city):
+    try:
+        new = request.get_json()
+    except:
+        return ("Not a JSON", 400)
+    for (k, v) in new.items():
+        if k != 'id' and k != 'created_at' and k != 'updated_at':
+            setattr(city, k, v)
+    return (city.to_dict(), 200)
+
+
+def deletecity(city):
+    storage.delete(city)
+    storage.reload()
+    return ({}, 200)
+
+
+@app_views.route('/cities', methods=['GET', 'POST'])
+def cities():
+    """  Retrieves list of all state objs"""
+    if request.method == 'GET':
+        all_cities = [x.to_dict() for x in storage.all('City').values()]
+        return (jsonify(all_cities), 200)
+    elif request.method == 'POST':
+        try:
+            new = request.get_json()
+        except:
+            return ("Not a JSON", 400)
+        if 'name' not in new.keys():
+            return ("Missing name", 400)
+        x = City()
+        for (k, v) in new.items():
+            setattr(x, k, v)
+        x.save()
+        return (x.to_dict(), 201)
+
+
+@app_views.route('/cities/<ident>', methods=['GET', 'PUT', 'DELETE'])
+def cities_id(ident):
+    """ """
+    cities = storage.all("City")
+    for c in cities:
+        if c.id == ident:
+            if request.method == 'GET':
+                return getcity(c)
+            elif request.method == 'PUT':
+                return putcity(c)
+            elif request.method == 'DELETE':
+                return deletecity(c)
+    return ({}, 404)
