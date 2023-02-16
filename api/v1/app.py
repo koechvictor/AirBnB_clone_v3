379a7fa10ee24ef91f8f9ab08e8e63616a63e8ab
victor koech <kipkiruiv408@gmail.com>
2023-02-16 09:01:55 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,14 @@
 starts a Flask web application
 """
 from flask import Flask, abort, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
@@ -16,12 +19,22 @@ def close(exc):
     """Calls storage close method"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(e):
-    """ Returns 404 error in JSON
-    """
+    """Returns 404 error in JSON"""
     e = {"error": "Not found"}
     return e, 404
+
+
+@app.errorhandler(400)
+def error_400(e):
+    """Returns 400 error in JSON"""
+    if not e:
+        e = {"error": "400"}
+    else:
+        return e, 400
+
 
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST"):
