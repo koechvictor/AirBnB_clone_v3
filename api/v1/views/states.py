864bfd2cc4a1bbaf6a43a23587142dce6e3d2154
victor koech <kipkiruiv408@gmail.com>
2023-02-18 08:29:24 +0000

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,34 +2,40 @@
 """
 Creates a new view for State objects for all default API actions
 """
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 
 
 def getstate(state):
-    """ """
-    return (state.to_dict(), 200)
+    """Get object"""
+    if state is None:
+        abort(404)
+    return (jsonify(state.to_dict()), 200)
 
 
 def putstate(state):
-    """ """
-    try:
-        new = request.get_json()
-    except:
-        return ("Not a JSON", 400)
+    """Update object"""
+    if state is None:
+        abort(404)
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+    new = request.get_json()
     for (k, v) in new.items():
         if k is not 'id' and k is not 'created_at' and k is not 'updated_at':
             setattr(state, k, v)
-        return (state.to_dict(), 200)
+    storage.save()
+    return (jsonify(state.to_dict()), 200)
 
 
 def deletestate(state):
-    """ """
+    """Delete object"""
+    if state is None:
+        abort(404)
     storage.delete(state)
-    storage.reload()
-    return ({}, 200)
+    storage.save()
+    return (jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['GET', 'POST'])
@@ -39,22 +45,21 @@ def states():
         all_states = [x.to_dict() for x in storage.all('State').values()]
         return (jsonify(all_states), 200)
     elif request.method == 'POST':
-        try:
-            new = request.get_json()
-        except:
-            return ("Not a JSON", 400)
+        if not request.is_json:
+            abort(400, 'Not a JSON')
+        new = request.get_json()
         if 'name' not in new.keys():
-            return ("Missing name", 400)
+            abort(400, 'Missing name')
         x = State()
         for (k, v) in new.items():
             setattr(x, k, v)
         x.save()
-        return (x.to_dict(), 201)
+        return (jsonify(x.to_dict()), 201)
 
 
 @app_views.route('/states/<ident>', methods=['GET', 'PUT', 'DELETE'])
 def states_id(ident):
-    """ """
+    """Retrieves a specific object """
     states = storage.all('State')
     for s in states.values():
         if s.id == ident:
@@ -64,4 +69,4 @@ def states_id(ident):
                 return putstate(s)
             elif request.method == 'DELETE':
                 return deletestate(s)
-    return ({}, 404)
+    abort(404, 'Not found')
